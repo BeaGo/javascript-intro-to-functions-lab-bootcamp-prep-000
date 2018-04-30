8addea6b69d9d71a09a819acc57bc9bfd89c6d7d
Bea Go <yobeago@gmail.com>
2018-04-30 03:06:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,3 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-var uppercase = "HELLO!"
-
-console.log(uppercase.toUpperCase() === uppercase)
-
-var lowercase = 'hello!'
-console.log(lowercase.toLowerCase() === lowercase)
